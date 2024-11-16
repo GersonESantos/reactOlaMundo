4053e814bed019fca2f29c4b856971b453643f0f
GersonESS <gersones@outlook.com>
2024-11-16 19:21:11 -0300

    Update App.jsx

--- projeto-olamundo/olamundo/src/App.jsx
+++ projeto-olamundo/olamundo/src/App.jsx
@@ -3,6 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function tick() {
+  const element = (
+    <div>
+      <h1>Olá, eu so um relogio</h1>
+      <h2>Agora são {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  )
+  root.render(element)
+  //return element
+}
+setInterval(tick, 1000)
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+const element = <h1>Olá, mundo!</h1>
+
+const name = 'GersonES'
+const pessoa = {  nome: 'Gerson', 
+                lastname: 'Santos',
+                idade: 67};
+
+const formataNome = (pessoa) => {
+  return pessoa.nome + ' ' + pessoa.lastname + ' ' + pessoa.idade
+}
 const eventobotao = () =>{
   
   alert('evento clicado')
@@ -13,17 +40,20 @@ const cliqueemmin = () =>
   }
 
 function App() {
-  const [count, setCount] = useState(0)
   
   return (
     <>  
       
       <div>
-        <h1>Olá, mundo!</h1>
+        {element}
+        Meu nome é {name}
         <p>Este é um projeto React com Vite.</p>
         <button onClick={(cliqueemmin)}>Clique Em Mim </button>
         <button onClick={(eventobotao)}>evento de botao</button>  
-      </div>
+        <br/>
+        Nome formatado: {formataNome(pessoa)}
+      
+        </div>
       
     </>
   )
